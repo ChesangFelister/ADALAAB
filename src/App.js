dc8d090e5c85b104e79ef1274470749f47af86dd
ChesangFelister <chesangfelister@gmail.com>
2020-02-27 14:09:06 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -1,41 +1,26 @@
 import React from "react";
 import "./App.css";
-import Home from"./Home";
+
 import Form from "./Form";
 import Login from "./Login";
 import { BrowserRouter as Router, Route , Link} from 'react-router-dom'
 
 
 class App extends React.Component {
-  // state = {
-  //   fields: {}
-  // };
-
-  // onChange = updatedValue => {
-  //   this.setState({
-  //     fields: {
-  //       ...this.state.fields,
-  //       ...updatedValue
-  //     }
-  //   });
-  // };
-
+ 
   render() {
     return (
       <div className="App">
       <Router>
        <Route exact path='/register' component={Form} />
-        <Route path='/login' component={Login} /> 
-        {/* <Route path='/' component={Home} />  */}
-        <Link to='/login'>Login </Link>
-        <Link to='/register'>Register</Link>
+        <Route path='/login' component={Login} />
+        <Link to='/login' style={{textDecoration:'none',marginRight:20,marginTop:'4rem'}}>Login </Link>
+        <Link to='/register' style={{textDecoration:'none',marginTop:'4rem'}}>Register</Link> 
+       
        </Router>
    
-        {/* <Form onChange={fields => this.onChange(fields)} />
-       
-        <Login/> 
-        <Home/> */}
-         
+        
+        
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,19 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// // import Login from "./Login";
-// // import { BrowserRouter, Route } from 'react-router-dom'
-
-// ReactDOM.render(
-//     <BrowserRouter>
-//       <div>
-//         <Route component={App} />
-        
-//         {/* <Route component={Login} />
-//         <Route path='/Login/:name' component={Login} /> */}
-//       </div>
-//     {/* </BrowserRouter>, */}
-// )
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
